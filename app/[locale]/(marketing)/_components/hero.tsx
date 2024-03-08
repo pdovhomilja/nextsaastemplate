8038b4b43f3ce6c57b220797045dfa1cfbaace6d
Pavel Dovhomilja <pavel@dovhomilja.cz>
2024-03-08 07:54:52 +0100

    fix: seo Title

--- app/[locale]/(marketing)/_components/hero.tsx
+++ app/[locale]/(marketing)/_components/hero.tsx
@@ -106,7 +106,7 @@ export const Header = () => {
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
         The Next.JS
-        <br /> SaaS Starter
+        <br /> SaaS Template
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         A modern Next.js starter with a minimalistic UI. Full Auth with
