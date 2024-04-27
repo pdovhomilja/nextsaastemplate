98e5fc88e291492a0f17c40d481f9c21b1f4ca98
Pavel Dovhomilja <pavel@dovhomilja.cz>
2024-04-27 10:40:22 +0200

    fix: homepage images

--- app/[locale]/(marketing)/page.tsx
+++ app/[locale]/(marketing)/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { HeroParallax } from "./_components/hero";
 import { unstable_setRequestLocale } from "next-intl/server";
@@ -16,47 +15,42 @@ export default function Home({
     {
       title: "Product 1",
       link: "/product/1",
-      thumbnail: "/product-1.jpg",
+      thumbnail: "/images/nextcrm-dark.png",
     },
     {
       title: "Product 2",
       link: "/product/2",
-      thumbnail: "/product-2.jpg",
+      thumbnail: "/images/nextcrm-light.png",
     },
     {
-      title: "Product 3",
-      link: "/product/3",
-      thumbnail: "/product-3.jpg",
+      title: "Product 1",
+      link: "/product/1",
+      thumbnail: "/images/nextcrm-dark.png",
     },
     {
-      title: "Product 4",
-      link: "/product/4",
-      thumbnail: "/product-4.jpg",
+      title: "Product 2",
+      link: "/product/2",
+      thumbnail: "/images/nextcrm-light.png",
     },
     {
-      title: "Product 5",
-      link: "/product/5",
-      thumbnail: "/product-5.jpg",
+      title: "Product 1",
+      link: "/product/1",
+      thumbnail: "/images/nextcrm-dark.png",
     },
     {
-      title: "Product 6",
-      link: "/product/6",
-      thumbnail: "/product-6.jpg",
+      title: "Product 2",
+      link: "/product/2",
+      thumbnail: "/images/nextcrm-light.png",
     },
     {
-      title: "Product 7",
-      link: "/product/7",
-      thumbnail: "/product-7.jpg",
+      title: "Product 1",
+      link: "/product/1",
+      thumbnail: "/images/nextcrm-dark.png",
     },
     {
-      title: "Product 8",
-      link: "/product/8",
-      thumbnail: "/product-8.jpg",
-    },
-    {
-      title: "Product 9",
-      link: "/product/9",
-      thumbnail: "/product-9.jpg",
+      title: "Product 2",
+      link: "/product/2",
+      thumbnail: "/images/nextcrm-light.png",
     },
   ];
   return (
